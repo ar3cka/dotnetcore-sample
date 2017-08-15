cddfc9a20c2e55c6751829f930abe351e40eb33a
anton mednonogov <mednonogov.anton@gmail.com>
2017-08-15 20:18:52 +0300

    Add Greeting service basic implemenation

---
+++ src/HowTo.DotNetCore2.GreetingService/Controllers/GreetingController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HowTo.DotNetCore2.GreetingService.Controllers
+{
+    [Route("api/v1/greeting")]
+    public class GreetingController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get(string name)
+        {
+            return Ok($"Hello, {name}!");
+        }
+    }
+}
